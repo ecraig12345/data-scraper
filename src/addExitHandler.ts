74fbfdec2eca21149e0462df78a0216697c78a69
Elizabeth Craig <elcraig@microsoft.com>
2019-05-17 12:16:33 -0700

    Better exit handler handling

--- src/addExitHandler.ts
+++ src/addExitHandler.ts
@@ -1,13 +1,30 @@
-process.setMaxListeners(40);
+const handlers: (() => any)[] = [];
 
 /**
  * Add a handler for various types of process exits.
  * Errors from this handler will be captured and logged.
  */
 export function addExitHandler(exitHandler: () => any) {
-  // https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
+  handlers.push(exitHandler);
+}
 
-  function safeExitHandler() {
+// https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
+
+// do something when app is closing
+process.on('exit', runHandlers);
+
+// catches ctrl+c event
+process.on('SIGINT', runHandlers);
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', runHandlers);
+process.on('SIGUSR2', runHandlers);
+
+// catches uncaught exceptions
+process.on('uncaughtException', runHandlers);
+
+function runHandlers() {
+  for (const exitHandler of handlers) {
     try {
       const result = exitHandler();
       if (result && typeof result === 'object' && (result as Promise<any>).then) {
@@ -19,17 +36,4 @@ export function addExitHandler(exitHandler: () => any) {
       console.log('Caught error in exit handler: ' + ex);
     }
   }
-
-  // do something when app is closing
-  process.on('exit', safeExitHandler);
-
-  // catches ctrl+c event
-  process.on('SIGINT', safeExitHandler);
-
-  // catches "kill pid" (for example: nodemon restart)
-  process.on('SIGUSR1', safeExitHandler);
-  process.on('SIGUSR2', safeExitHandler);
-
-  // catches uncaught exceptions
-  process.on('uncaughtException', safeExitHandler);
 }
